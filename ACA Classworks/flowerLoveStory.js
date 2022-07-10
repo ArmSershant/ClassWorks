40d264d35bc79519581bcfc2f921b4b19b8f5deb
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-07-10 14:55:33 +0400

    flower index

--- ACA Classworks/flowerLoveStory.js
+++ ACA Classworks/flowerLoveStory.js
@@ -18,6 +18,11 @@ function Person(name , gender) {
     };
 }
 
+function Flower(type, color){
+    this.type = type;
+    this.color = color;
+}
+
 function FlowerShop(flowers) {
     this.flowers = [
         new Flower("rose", "red"),
@@ -25,19 +30,30 @@ function FlowerShop(flowers) {
         new Flower("lily", "blue")
     ];
     this.sellFlower = function(flower) {
+        index = this.findFlowerIndex(color,type);
+        return this.giveFlower(index);
         return this.flowers.pop(flower);
     }
+    this.findFlowerIndex = function(color, type){
+        for (flower of this.flowers) {
+        return this.flowers.findIndex(flower => flower.color === color && flower.type === type);
+        };
+    }
+    this.giveFlower = function(index) {
+        return this.flowers.splice(index, 1);
+    };
 }
 
-function Flower(type,color){
-    this.type = type;
+function Order(color, type) {
     this.color = color;
+    this.type = type;
 }
+
 const boy = new Person("Vardges", "male");
 const girl = new Person("Mariam", "female");
 
 const flowerShop = new FlowerShop();
-const flower = flowerShop.sellFlower();
+const flower = flowerShop.sellFlower("red", "rose");
 boy.buyFlower(flower);
 const gift = boy.giveFlower(flower);
 girl.takeFlower(flower)
