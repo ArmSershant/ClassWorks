0bb95fe37e2f6e67c3f95cec9915d894eb90eebc
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-07-10 16:59:16 +0400

    Added id checker

--- ACA Classworks/findFriends.js
+++ ACA Classworks/findFriends.js
@@ -1,12 +1,15 @@
 function Person(id, name){
+    this.id = id;
     this.name = name;
     this.friends = [];
     this.hasFriend = function(person) {
         return this.friends.some(elem => elem.id === person.id)
     }
     this.addfriend = function(friend) {
+        if (!this.hasFriend(friend)) {
         this.friends.push(friend);
         console.log( "Friend " + `${friend.name}`+ " added " + "to " + `${this.name}` + " friend list");
+        };
     };
 }
 
@@ -14,4 +17,4 @@ const John = new Person(1, "John");
 const Mary = new Person(2, "Mary");
 const Mark = new Person(3, "Mark");
 Mark.addfriend(Mary);
-console.log(Mark.hasFriend(Mary));
+console.log(Mark.hasFriend(John));
