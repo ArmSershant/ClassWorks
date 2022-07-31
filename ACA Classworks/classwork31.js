15c3e7c4ef38b9b979254e27c15f4a5a86e05e2d
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-07-31 14:33:32 +0400

    Notifications solved by myself

--- ACA Classworks/classwork31.js
+++ ACA Classworks/classwork31.js
@@ -12,25 +12,68 @@
 // }
 // developer.writeCode();
 // human.sayName();
-class Human {
-    constructor(name) {
-        this.name = name;
+
+// class Human {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     sayName() {
+//         console.log(this.name);
+//     }
+// }
+// class Developer extends Human {
+//     constructor(name, language) {
+//         super(name);
+//         this.language = language;
+//     }
+//     writeCode() {
+//         console.log("Writing code");
+//     }
+// }
+// const developer = new Developer('Edgar', 'JavaScript');
+// developer.sayName();
+// developer.writeCode();
+// console.log(developer);
+// create with class constructor set notification (alert,mail,message,send() etc) and use getTitle getBody methods.
+class Notification {
+    constructor(title, body) {
+        this.title = title;
+        this.body = body;
+        // this.send = send;
     }
-    sayName() {
-        console.log(this.name);
+    getTitle() {
+        return "Title";
+    }
+    getBody() {
+        return "Body";
+    }
+    send() {
+        if(!this.body){
+            return console.log("No body");
+        }
+        if (this.title) {
+        console.log(`Sending ${this.title}`,`${this.body}`);
+        } else {
+            console.log('Notification is empty');
+        }
     }
 }
-class Developer extends Human {
-    constructor(name, language) {
-        super(name);
-        this.language = language;
+const notification = new Notification('Title', 'Body');
+notification.send();
+class EmailNotification extends Notification {
+    getTitle() {
+        return "Email Subject";
     }
-    writeCode() {
-        console.log("Writing code");
+    getBody() {
+        return "Email Body";
     }
 }
-const developer = new Developer('Edgar', 'JavaScript');
-developer.sayName();
-developer.writeCode();
-console.log(developer);
-// create with class constructor set notification (alert,mail,message etc) and use getTitle getBody methods.
+const email = new EmailNotification();
+email.send();
+class SMSNotification extends Notification {
+    getBody() {
+        return "SMS Body";
+    }
+}
+const sms = new SMSNotification();
+sms.send();
