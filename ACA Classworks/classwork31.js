79a5572fc92e93fec1924c8c37e76ed0f7cb4faf
Vardges <101866789+ArmSershant@users.noreply.github.com>
2022-07-31 12:33:49 +0400

    -_-

--- ACA Classworks/classwork31.js
+++ ACA Classworks/classwork31.js
@@ -1,14 +1,36 @@
-const human = {
-    name: 'John',
-    sayName: function () {
+// const human = {
+//     name: 'John',
+//     sayName: function () {
+//         console.log(this.name);
+//     }
+// }
+// let developer = {
+//     name : 'Edgar',
+//     writeCode: function () {
+//         console.log("Writing code");
+//     }
+// }
+// developer.writeCode();
+// human.sayName();
+class Human {
+    constructor(name) {
+        this.name = name;
+    }
+    sayName() {
         console.log(this.name);
     }
 }
-let developer = {
-    name : 'Edgar',
-    writeCode: function () {
+class Developer extends Human {
+    constructor(name, language) {
+        super(name);
+        this.language = language;
+    }
+    writeCode() {
         console.log("Writing code");
     }
 }
+const developer = new Developer('Edgar', 'JavaScript');
+developer.sayName();
 developer.writeCode();
-human.sayName(); 
+console.log(developer);
+// create with class constructor set notification (alert,mail,message etc) and use getTitle getBody methods.
